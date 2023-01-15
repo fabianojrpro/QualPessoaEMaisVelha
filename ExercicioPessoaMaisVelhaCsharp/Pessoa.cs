ad25c4b584036d356926c8f2fe3934d417b9c010
Fabiano Junior <99301175+fabianojrpro@users.noreply.github.com>
2023-01-15 20:21:33 -0300

    Add files via upload

---
+++ ExercicioPessoaMaisVelhaCsharp/Pessoa.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioPessoaMaisVelhaCsharp
+{
+    internal class Pessoa
+    {
+        public string nome { get; private set; }
+        public int idade { get; private set; }
+
+        public Pessoa(string nome, int idade)
+        {
+            this.nome = nome;
+            this.idade = idade;
+        }
+
+        public void MaisVelha(Pessoa um, Pessoa dois)
+        {
+            if (um.idade > dois.idade)
+            {
+                Console.WriteLine("Pessoa mais velha: " + um.nome);
+            }
+            else
+            {
+                Console.WriteLine("Pessoa mais velha: " + dois.nome);
+            }
+        }
+    }
+}
---
+++ ExercicioPessoaMaisVelhaCsharp/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ExercicioPessoaMaisVelhaCsharp;
+
+Console.WriteLine("Dados da primeira pessoa: ");
+Console.Write("Nome: ");
+string nomeDaPrimeira = Console.ReadLine();
+Console.Write("Idade: ");
+int idadeDaPrimeira = int.Parse(Console.ReadLine());
+Pessoa primeira = new Pessoa(nomeDaPrimeira, idadeDaPrimeira);
+
+Console.WriteLine("Dados da segunda pessoa: ");
+Console.Write("Nome: ");
+string nomeDaSegunda = Console.ReadLine();
+Console.Write("Idade: ");
+int idadeDaSegunda = int.Parse(Console.ReadLine());
+Pessoa segunda = new Pessoa(nomeDaSegunda, idadeDaSegunda);
+
+primeira.MaisVelha(primeira,segunda);
